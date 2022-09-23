8e048a40b6ac032e21e4868d9d90e061e1fb43fd
Tohid Kovadiya <kovadiya.tohid@gmail.com>
2022-09-23 12:36:08 +0530

    Basic setup done

---
+++ android/app/src/main/kotlin/com/tohid/flutter/first_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tohid.flutter.first_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home : Scaffold(
+    appBar: AppBar(
+      title: const Text('Hello This is my first Flutter App'),
+      centerTitle: true,
+    ),
+  ),
+));
+
